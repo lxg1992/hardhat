6477befecc54a1b3f50c12110063738272fff520
Alexey <lxg1992@gmail.com>
2022-10-31 15:54:25 -0400

    Stuff

---
+++ truffle/test/Spacebear.test.js
@@ -0,0 +1,15 @@
+const Spacebear = artifacts.require("Spacebear");
+const truffleAssert = require('truffle-assertions');
+
+contract("Spacebear", (accounts) => {
+    it('should credit NFT to a specific account', async () => {
+        const spacebearInstance = await Spacebear.deployed();
+        let txResult = await spacebearInstance.safeMint(accounts[1], "spacebear_1.json");
+
+        truffleAssert.eventEmitted(txResult, 'Transfer', { from: '0x0000000000000000000000000000000000000000', to: accounts[1], tokenId: web3.utils.toBN("0") })
+
+
+        console.log(spacebearInstance.ownerOf(0));
+        assert.equal(await spacebearInstance.ownerOf(0), accounts[1], "Owner of token is the wrong address");
+    }) 
+})
